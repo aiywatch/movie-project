79bcf55bf56063f95ade8ae0dee95583018158ce
aiywatch <aiycp35@gmail.com>
2016-07-19 05:01:21 +0000

    tutorial

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+
+
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'movies#index'
+end
