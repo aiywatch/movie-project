19ac6a2fa48342e346bf546e9712a124c735d124
aiywatch <aiycp35@gmail.com>
2016-07-19 21:13:54 +0000

    admin namespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def admin_access
+    if !current_user || current_user.role != 'admin'
+      flash[:alert] = "You must be admin."
+      redirect_to root_path
+    end
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
