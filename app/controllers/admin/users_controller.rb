d0036fed34d8c219029cfdae92bd60ee2ec156ba
aiywatch <aiycp35@gmail.com>
2016-07-19 23:03:43 +0000

    admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,11 +3,19 @@ class Admin::UsersController < ApplicationController
   before_filter :admin_access
 
   def index
-    @users = User.all
+    @users = User.page(params[:page]).per(5)
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -20,4 +28,26 @@ class Admin::UsersController < ApplicationController
       render :new
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path, notice: "updated"
+    else
+      render :show, notice: "error"
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to admin_users_path, notice: "deleted"
+  end
+
+  protected
+    def user_params
+      params.require(:user).permit(
+        :email, :firstname, :lastname, :role
+      )
+    end
 end
